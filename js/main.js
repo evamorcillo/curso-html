66c4bf721ad9aac168a314b6816ba43eba98fb18
Eva Morcillo <emorcillorodriguez@gmail.com>
2016-07-25 20:55:11 +0200

    responsive 600px

--- js/main.js
+++ js/main.js
@@ -4,4 +4,8 @@ document.querySelector(".botonmenu").addEventListener("click", function(){
 
 document.querySelector(".cerrar").addEventListener("click", function(){
     document.querySelector(".menu").classList.remove("visible");
-})
+});
+
+ document.querySelector(".menu").addEventListener("click", function(){
+    document.querySelector(".menu").classList.remove("visible");
+});
